5ad14db118c12a285a24e73e26b9e3185937cd45
Vladimir Lapin <51967175+voldyman13@users.noreply.github.com>
2022-03-29 00:15:40 +0300

    First commit. Creating a project

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+OTUS PROJECT
+Homework 1
+Vladimir Lapin
+28.03.2022
+"""
+
+
+def print_hello(name):
+    print(f'Hello {name}!')  # Press ⌘F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hello('Word')
+
