9c3ce8db1517e0c7c6c151ca4bb5476d276c9cc9
Vladimir Lapin <51967175+voldyman13@users.noreply.github.com>
2022-03-30 12:50:10 +0300

    added requirements

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ Vladimir Lapin
 28.03.2022
 """
 
-
-def print_hello(name):
-    print(f'Hello {name}!')  # Press ⌘F8 to toggle the breakpoint.
+print(f'Hello Word')
 
 
-if __name__ == '__main__':
-    print_hello('Word')
+
 
